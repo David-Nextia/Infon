96fbdd470090debce9d7d2cf9c9c1cbdeb2345b3
David-Nextia <david.llanes@nextia.mx>
2020-09-22 18:58:40 -0500

    Creditos fragmetn recibiendo información y probando tabs

--- app/src/main/java/com/nextia/micuentainfonavit/ui/saldo/SaldoFragment.java
+++ app/src/main/java/com/nextia/micuentainfonavit/ui/saldo/SaldoFragment.java
@@ -2,11 +2,15 @@ package com.nextia.micuentainfonavit.ui.saldo;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -27,19 +31,26 @@ public class SaldoFragment extends Fragment implements OnFinishRequestListener<S
     private SaldoViewModel saldoViewModel;
     public TextView ahorro;
     ProgressBar progres;
+    LinearLayout linear;
+    TabLayout tabLayout;
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         saldoViewModel =new ViewModelProvider(this).get(SaldoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_saldos, container, false);
         Utils utils= new Utils();
         ahorro=root.findViewById(R.id.txtAhorroTotal);
         progres= root.findViewById(R.id.progressBarSaldos);
+        linear=root.findViewById(R.id.linearopaque);
         progres.setAlpha(1.0f);
-        TabLayout tabLayout;
+
         tabLayout=(TabLayout)root.findViewById(R.id.tabs);
         tabLayout.addTab(tabLayout.newTab().setText("Home"));
         tabLayout.addTab(tabLayout.newTab().setText("Sport"));
         tabLayout.addTab(tabLayout.newTab().setText("Movie"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        linear.setAlpha(0.3f);
+        tabLayout.setClickable(false);
+        this.getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
        /* SharedPreferences  mPrefs = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = mPrefs.edit();
         Gson gson = new Gson();
@@ -58,8 +69,13 @@ public class SaldoFragment extends Fragment implements OnFinishRequestListener<S
 
     @Override
     public void onSuccesRequest(SaldoResponse object) {
-        ahorro.setText("$"+object.getSaldoSARTotal().toString());
+        this.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
         progres.setAlpha(0.0f);
+        linear.setAlpha(1.0f);
+        tabLayout.setClickable(true);
+
+        ahorro.setText("$"+object.getSaldoSARTotal().toString());
+
 
     }
 }
