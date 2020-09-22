9d8aabb03fb8b1f1f8a0760231ccdce55540d7cb
David-Nextia <david.llanes@nextia.mx>
2020-09-22 17:32:49 -0500

    funciones en vista saldos

--- app/src/main/java/com/nextia/micuentainfonavit/ui/saldo/SaldoViewModel.java
+++ app/src/main/java/com/nextia/micuentainfonavit/ui/saldo/SaldoViewModel.java
@@ -1,8 +1,15 @@
 package com.nextia.micuentainfonavit.ui.saldo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.gson.Gson;
+import com.nextia.domain.OnFinishRequestListener;
+import com.nextia.domain.models.user.UserResponse;
 
 public class SaldoViewModel extends ViewModel {
 
@@ -13,7 +20,18 @@ public class SaldoViewModel extends ViewModel {
 
         mText.setValue("fragmento cuanto ahorro tengo");
     }
-    public void getUserSharePrefs(){
+    public void getUserSharePrefs(OnFinishRequestListener listener){
+        Context context= (Context)listener;
+
+    }
+
+    public UserResponse getSharedPreferences(Context context){
+        SharedPreferences  mPrefs = context.getSharedPreferences("pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = mPrefs.getString("UsuarioData", "");
+        UserResponse obj = gson.fromJson(json, UserResponse.class);
+        return obj;
 
     }
 
